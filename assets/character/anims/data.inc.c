18624dc0ce10cbd10c282945f2e3f6a6a16d7e45
InTheBeef <inthebeef@gmail.com>
2020-11-28 20:11:10 -0600

    New Animations
    Idle_Long: Will create more variants later but we should always start with this idle.
    Jump_Start: Not sure if it'll look great, might need to do something about entering jump from a run.
    Walking: Minor tweak, probably going to tweak again later.

--- assets/character/anims/data.inc.c
+++ assets/character/anims/data.inc.c
@@ -1,2 +1,4 @@
 #include "anim_Action.inc.c"
 #include "anim_Walking.inc.c"
+#include "anim_Jump_Start.inc.c"
+#include "anim_Idle_Long.inc.c"
--- assets/character/anims/table.inc.c
+++ assets/character/anims/table.inc.c
@@ -3,5 +3,9 @@ const struct Animation *const character_anims[] = {
 
 	&character_anim_Walking,
 
+	&character_anim_Jump_Start,
+
+	&character_anim_Idle_Long,
+
 	NULL,
 };

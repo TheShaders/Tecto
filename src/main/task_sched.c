ff48d1778b617348372627fb079950dfa8793d94
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2020-12-14 00:40:34 -0500

    PAL hopefully

--- src/main/task_sched.c
+++ src/main/task_sched.c
@@ -17,19 +17,55 @@ static u64 schedStack[OS_SC_STACKSIZE / sizeof(u64)];
 void initScheduler(void)
 {
     u8 mode;
-    #ifdef HIGH_RES
-    #ifdef INTERLACED
-        mode = OS_VI_NTSC_HAN1;
-    #else
-        mode = OS_VI_NTSC_HAF1;
-    #endif
-    #else
-    #ifdef INTERLACED
-        mode = OS_VI_NTSC_LAF1;
-    #else
-        mode = OS_VI_NTSC_LAN1;
-    #endif
-    #endif
+    switch (osTvType)
+    {
+        case OS_TV_PAL:
+            #ifdef HIGH_RES
+            #ifdef INTERLACED
+                mode = OS_VI_FPAL_HAN1;
+            #else
+                mode = OS_VI_FPAL_HAF1;
+            #endif
+            #else
+            #ifdef INTERLACED
+                mode = OS_VI_FPAL_LAF1;
+            #else
+                mode = OS_VI_FPAL_LAN1;
+            #endif
+            #endif
+            break;
+        case OS_TV_MPAL:
+            #ifdef HIGH_RES
+            #ifdef INTERLACED
+                mode = OS_VI_MPAL_HAN1;
+            #else
+                mode = OS_VI_MPAL_HAF1;
+            #endif
+            #else
+            #ifdef INTERLACED
+                mode = OS_VI_MPAL_LAF1;
+            #else
+                mode = OS_VI_MPAL_LAN1;
+            #endif
+            #endif
+            break;
+        case OS_TV_NTSC:
+        default:
+            #ifdef HIGH_RES
+            #ifdef INTERLACED
+                mode = OS_VI_NTSC_HAN1;
+            #else
+                mode = OS_VI_NTSC_HAF1;
+            #endif
+            #else
+            #ifdef INTERLACED
+                mode = OS_VI_NTSC_LAF1;
+            #else
+                mode = OS_VI_NTSC_LAN1;
+            #endif
+            #endif
+            break;
+    }
     #ifdef FPS30
     osCreateScheduler(&scheduler, &schedStack[OS_SC_STACKSIZE / sizeof(u64)], SCHEDULER_PRI, mode, 2);
     #else
